68ed2b1058ee25561ade58097f790125d7b2cc27
Xavier <x.crochet@gmail.com>
2020-06-11 11:06:40 +0200

    Fin quête

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SignupComponent } from './pages/signup/signup.component';
+import { UserComponent } from './pages/user/user.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'user', pathMatch: 'full'},
+  {path: 'signup', component: SignupComponent, pathMatch: 'full'},
+  {path: 'user', component: UserComponent, pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
